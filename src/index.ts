81beeee32df196271fdcbc65a72168c7deb7b944
JounQin <admin@1stg.me>
2022-07-11 00:22:34 +0800

    feat: first blood, should just work

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-export default () => 'Hello World!'
+// eslint-disable-next-line @typescript-eslint/no-explicit-any -- it's fine
+export class TTLCache<K extends keyof any, V = unknown> {
+  private readonly ttlMap = new Map<K, number>()
+  private readonly valueMap = new Map<K, V | undefined>()
+
+  constructor(private readonly ttl = 1000) {}
+
+  get(key: K, setter?: (value?: V) => V) {
+    const now = performance.now()
+    const ttl = this.ttlMap.get(key)
+    let value = this.valueMap.get(key)
+    if (!ttl || value == null || now > ttl) {
+      this.ttlMap.set(key, now + this.ttl)
+      this.valueMap.set(key, (value = setter?.(value)))
+    }
+    return value
+  }
+
+  set(key: K, value: V) {
+    this.ttlMap.set(key, performance.now() + this.ttl)
+    this.valueMap.set(key, value)
+  }
+}
--- test/basic.spec.ts
+++ test/basic.spec.ts
@@ -1,5 +1,22 @@
-import echo from 'lib-boilerplate'
+import { setTimeout } from 'node:timers/promises'
 
-test('it should just work', () => {
-  expect(echo()).toBe('Hello World!')
+import { TTLCache } from 'yocto-ttl-cache'
+
+test('basic usage', async () => {
+  const cache = new TTLCache<string, string>()
+  expect(cache.get('key')).toBe(undefined)
+  cache.set('key', 'value')
+  expect(cache.get('key')).toBe('value')
+  expect(cache.get('key')).toBe('value')
+  await setTimeout(1000)
+  expect(cache.get('key')).toBe(undefined)
+})
+
+test('get with setter', async () => {
+  const cache = new TTLCache<string, string>()
+  expect(cache.get('key')).toBe(undefined)
+  expect(cache.get('key', () => 'value')).toBe('value')
+  expect(cache.get('key', () => 'unused')).toBe('value')
+  await setTimeout(1000)
+  expect(cache.get('key')).toBe(undefined)
 })
